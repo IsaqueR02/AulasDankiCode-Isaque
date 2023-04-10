72c9ccfaccee71bd6c1761dd503ba77657a58fa5
IsaqueR02 <71456433+IsaqueR02@users.noreply.github.com>
2023-04-10 09:27:50 -0300

    Update TestAbstract.java

--- src/oo/TestAbstract.java
+++ src/oo/TestAbstract.java
@@ -1,9 +1,13 @@
 package oo;
 
-abstract class TestAbstract {
+abstract/* classe abstrata, é uma classe que não aparece se não for chamada*/ class TestAbstract extends Enemy{
 
 	
 	public void iniciarJogo() {
 		System.out.println("Iniciando Jogo....");
 	}
+	public void inimigo() {
+		this.iniciarInimigo();
+	}
 }
+
